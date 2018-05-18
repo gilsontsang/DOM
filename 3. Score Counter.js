1c12dd3060ef8c1143deed27d64dce225f071548
Gilson Tsang <37494996+gilsontsang@users.noreply.github.com>
2018-05-17 17:01:07 -0700

    Update 3. Score Counter.js

--- 3. Score Counter.js
+++ 3. Score Counter.js
@@ -15,37 +15,37 @@ var compareNumbers = 5;
 // Sets how many points the game goes up to.
 input.addEventListener("change", function() {
 	playUpTo.textContent = input.value;
-  compareNumbers = Number(input.value);
+	compareNumbers = Number(input.value);
 });
 
 // For each click... the numbers go up for player one. Wow!
 playerOneButton.addEventListener("click", function() {
 	if (playerOneCounter < compareNumbers && playerTwoCounter !== compareNumbers) {
-  	playerOneCounter++;
-  	playerOneScore.textContent = playerOneCounter;
-  }	
-  if (playerOneCounter === compareNumbers) {
-  	playerOneScore.style.color = "green";
-  }
+  		playerOneCounter++;
+  		playerOneScore.textContent = playerOneCounter;
+  	}	
+  	if (playerOneCounter === compareNumbers) {
+  		playerOneScore.style.color = "green";
+  	}
 });
 
 // For each click... the numbers go up for player two. Wow!
 playerTwoButton.addEventListener("click", function() {
 	if (playerTwoCounter < compareNumbers && playerOneCounter !== compareNumbers) {
 		playerTwoCounter++;
-  	playerTwoScore.textContent = playerTwoCounter;
-  }
-  if (playerTwoCounter === compareNumbers) {
-  	playerTwoScore.style.color = "green";
-  }
+  		playerTwoScore.textContent = playerTwoCounter;
+  	}
+	if (playerTwoCounter === compareNumbers) {
+  		playerTwoScore.style.color = "green";
+  	}
 });
 
 // Resets game
 reset.addEventListener("click", function() {
 	playerOneScore.textContent = 0;
-  playerTwoScore.textContent = 0;
-  playerOneCounter = 0;
-  playerTwoCounter = 0;
-  playerOneScore.style.color = "black";
-  playerTwoScore.style.color = "black";
+	playerTwoScore.textContent = 0;
+	playerOneCounter = 0;
+	playerTwoCounter = 0;
+	playerOneScore.style.color = "black";
+	playerTwoScore.style.color = "black";
 });

8b55dc7bff678b59ef448fec311dc0a1d78347ff
Gilson Tsang <37494996+gilsontsang@users.noreply.github.com>
2018-05-25 12:23:10 -0700

    Update 4. Color Game.js

--- 4. Color Game.js
+++ 4. Color Game.js
@@ -1,27 +1,57 @@
+var numSquares = 6;
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
-var colors = generateRandomColors(6);
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.querySelector("#colorDisplay");
 var pickedColor = pickColor();
 var messageDisplay = document.querySelector("#message");
-var selected = document.querySelector(".selected");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 
-selected.addEventListener("click", function() {
-	
+easyBtn.addEventListener("click", function() {
+	hardBtn.classList.remove("selected");
+	easyBtn.classList.add("selected");
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+  	if(colors[i]) {
+    	squares[i].style.background = colors[i];
+    }
+    else {
+    	squares[i].style.display = "none";
+    }
+  }
+});
+
+hardBtn.addEventListener("click", function() {
+	easyBtn.classList.remove("selected");
+	hardBtn.classList.add("selected");
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+  	squares[i].style.background = colors[i];
+ 		squares[i].style.display = "block";
+  }
 });
 
 resetButton.addEventListener("click", function() {
 	//generate all new colors
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numSquares);
   // change colorDisplay to match picked Color
   colorDisplay.textContent = pickedColor;
   //pick a new random color from array
   pickedColor = pickColor();
   //reset h1 color back to background
-  h1.style.background = "#232323";
+  h1.style.background = "steelblue";
   //reset Reset button back to New colors
-  resetButton.textContent = "New colors"
+  this.textContent = "New colors"
+    //reset message display
+  messageDisplay.textContent = "";
   //change colors of squares
   mainProcess();
 })
@@ -36,49 +66,24 @@ function mainProcess() {
 	for (var i=0; i<squares.length; i++) {
 	// add initial colors to squares
 	squares[i].style.backgroundColor = colors[i];
-
 	// add clicklisteners to squares
 	squares[i].addEventListener("click", function() {
   	// compare color to pickedColor
   	var clickedColor = this.style.backgroundColor;
   	// cinoare color to pickedColor
-	if(clickedColor === pickedColor) {
-		messageDisplay.textContent = "Correct";
-    changeColors(clickedColor);
-    h1.style.background = clickedColor;
-    resetButton.textContent = "Play Again?"
+		if(clickedColor === pickedColor) {
+      messageDisplay.textContent = "Correct";
+      changeColors(clickedColor);
+      h1.style.background = clickedColor;
+      resetButton.textContent = "Play Again?"
     }
-  	else {
-    		this.style.backgroundColor = "#232323";
-      	messageDisplay.textContent = "Try Again";
+    else {
+        this.style.backgroundColor = "#232323";
+        messageDisplay.textContent = "Try Again";
     }
-  })
+   })
 	}
 }
-
-/*
-for (var i=0; i<squares.length; i++) {
-	// add initial colors to squares
-	squares[i].style.backgroundColor = colors[i];
-
-	// add clicklisteners to squares
-	squares[i].addEventListener("click", function() {
-  	// compare color to pickedColor
-  	var clickedColor = this.style.backgroundColor;
-  	// cinoare color to pickedColor
-	if(clickedColor === pickedColor) {
-		messageDisplay.textContent = "Correct";
-    changeColors(clickedColor);
-    h1.style.background = clickedColor;
-    	}
-  	else {
-    		this.style.backgroundColor = "#232323";
-      		messageDisplay.textContent = "Try Again";
-    	}
-  })
-}
-*/
-
 
 function changeColors(color) {
 	// loop through all squares
